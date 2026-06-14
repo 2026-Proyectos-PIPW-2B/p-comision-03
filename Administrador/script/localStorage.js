c69fd6f2859f0184fd04846fc24d6820e3c9debb
Camila Corbatta <cami24704@gmail.com>
2026-06-14 02:43:11 -0300

    agregar resgistroUS

--- Administrador/script/localStorage.js
+++ Administrador/script/localStorage.js
@@ -1,9 +1,9 @@
-export function obtenerCategorias(){
-    const categorias = localStorage.getItem("bd-categorias")
+export function obtenerLocalStorage(bd){
+    const bd_actual = localStorage.getItem(bd)
     let resultado
 
-    if (categorias) {
-        resultado = JSON.parse(categorias)
+    if (bd_actual) {
+        resultado = JSON.parse(bd_actual)
     } else {
         resultado = []
     }
@@ -11,6 +11,6 @@ export function obtenerCategorias(){
     return resultado
 }
 
-export function guardarCategorias(categorias){
-    localStorage.setItem("bd-categoria", JSON.stringify(categorias))
+export function guardarLocalStorage(bd_actual, bd){
+    localStorage.setItem(bd, JSON.stringify(bd_actual))
 }
